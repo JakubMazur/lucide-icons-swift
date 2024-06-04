0d854c80172bbe9ca05aae15aa6b61477bf27f38
Jakub Mazur <JakubMazur@users.noreply.github.com>
2024-06-04 13:51:57 +0200

    Fetch Lucide Release yml
    Update main.yml
    
    Create checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update previous release
    
    Update checkLucideReleaseCron.yml
    
    Delete .previous_release
    
    Update previous release
    
    Update checkLucideReleaseCron.yml
    
    Update previous release
    
    Update previous release
    
    Update checkLucideReleaseCron.yml
    
    add icons
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update icons to release 0.383.0
    
    Update checkLucideReleaseCron.yml
    
    Create kuba.svg
    
    Create akuba.svg
    
    Update icons to release 0.383.0
    
    Delete Sources/raw/a-arrow-down.svg
    
    Update icons to release 0.383.0
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update icons to release 0.383.0
    
    updates
    
    Update checkLucideReleaseCron.yml
    
    tesyt
    
    Update checkLucideReleaseCron.yml
    
    Update icons to release 0.383.0
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Process SVGs into Xcode asset catalog for release 0.383.0
    
    Update checkLucideReleaseCron.yml
    
    remove raw files
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    0.383.0
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    0.383.0
    
    Update checkLucideReleaseCron.yml
    
    0.383.0
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    0.383.0
    
    Update checkLucideReleaseCron.yml
    
    0.383.0
    
    Update checkLucideReleaseCron.yml
    
    Update checkLucideReleaseCron.yml
    
    0.383.0
    
    Update checkLucideReleaseCron.yml
    
    0.383.0
    
    Delete .github/workflows/main.yml
    
    Rename checkLucideReleaseCron.yml to main.yml
    
    Update main.yml
    
    0.3.3
    
    Update main.yml
    
    0.383.0
    
    0.382.0
    
    Update main.yml
    
    0.383.0
    
    1
    
    Update main.yml
    
    0.383.0
    
    Delete .swiftpm/xcode/package.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist
    
    0.382.0
    
    0.383.0

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "LucideIcons",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_13)
+	],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LucideIcons",
             targets: ["LucideIcons"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LucideIcons"),
+            name: "LucideIcons",
+			resources: [
+				.process("icons.xcassets")
+			]
+		),
         .testTarget(
             name: "LucideIconsTests",
             dependencies: ["LucideIcons"]),
---
+++ Sources/LucideIcons/Lucide.swift
@@ -0,0 +1,51 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+//#if canImport(UIKit)
+//import UIKit
+//
+//public struct Lucide {
+//
+//	public static let example: UIImage = UIImage(named: "example", in: Bundle.module, compatibleWith: nil)!
+//
+//	public init() {
+//
+//	}
+//	
+//	
+//}
+//#endif
+//
+//#if canImport(UIKit)
+//import UIKit
+//#endif
+//
+//#if canImport(AppKit)
+//import AppKit
+//#endif
+//
+//public class Lucide {
+//	public func example() -> NSImage {
+//		let image = Bundle.module.image(forResource: "tv")
+//		return image
+//		guard let image = Bundle(for: type(of: self)).image(forResource: "tv") else {
+//			fatalError("Test Resource is Missing.")
+//		}
+//		return image
+//	}
+//	
+//	
+//	#if canImport(AppKit)
+//	
+//	
+//	
+//	#endif
+//	
+////	public init() { }
+//
+//}
+
+
+public struct Lucide {
+	
+}
